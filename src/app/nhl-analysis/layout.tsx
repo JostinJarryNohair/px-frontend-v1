0c3c26a19ac0f7b10904de570d916c70d701959d
JostinJarryNohair <jostin@luxevotech.com>
2025-03-17 10:49:30 -0400

    Added the MainLayout wrapp in the root layout to prevent from addind it inside nested components

--- src/app/nhl-analysis/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/components/layout/MainLayout";
-
-export default function NHLAnalysisLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <MainLayout>{children}</MainLayout>;
-}
